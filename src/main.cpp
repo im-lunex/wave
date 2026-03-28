c950c4bd0059c8448007980cd4937a662514ccd3
im-lunex <thississamir04@gmail.com>
2026-03-28 14:15:59 +0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,27 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr, ooowww maaa ga
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	// Flush after every std::cout / std:cerr, ooowww maaa ga
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-  while (true) {
-    std::cout << "$ "; // show the prompt, wtf
+	while (true) {
+		std::cout << "$ "; // show the prompt, wtf
 
-    std::string input;
-    if (!std::getline(std::cin, input)) {
-      break; // holly shit, input ended
-    }
+		std::string input;
+		if (!std::getline(std::cin, input)) {
+			break; // holly shit, input ended
+		}
 
-    if (input == "exit 0") {
-      return 0; // gtfo, we are done here
-    }
+		if (input == "exit") {
+			return 0; // gtfo, we are done here
+		}
 
-    if (!input.empty()) {
-      std::cout << input << ": command not found" << std::endl; // shit, command is missing
-    }
-  }
-  
-  return 0;
+		if (!input.empty()) {
+			std::cout << input << ": command not found"
+					  << std::endl; // shit, command is missing
+		}
+	}
+
+	return 0;
 }
