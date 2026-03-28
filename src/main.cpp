ba4fa9bbb519cbb6c02ce03f6200e74c731e131c
im-lunex <thississamir04@gmail.com>
2026-03-28 14:28:28 +0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <filesystem>
-#include <cstdlib>
+
+#include <unistd.h>
 
 namespace fs = std::filesystem;
 
@@ -16,42 +18,44 @@ std::string find_path(const std::string& command) {
     std::string path;
     while (std::getline(ss, path, ':')) {
         fs::path p = fs::path(path) / command;
-        if (fs::exists(p)) return p.string();
+        if (fs::exists(p) && access(p.c_str(), X_OK) == 0) return p.string();
     }
     return "";
 }
 
 int main() {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-  while (true) {
-    std::cout << "$ ";
+	while (true) {
+		std::cout << "$ ";
 
-    std::string input;
-    if (!std::getline(std::cin, input)) break;
-    if (input.empty()) continue;
+		std::string input;
+		if (!std::getline(std::cin, input))
+			break;
+		if (input.empty())
+			continue;
 
-    if (input == "exit 0" || input == "exit") {
-      return 0; // gtfo
-    } else if (input.substr(0, 5) == "echo ") {
-      std::cout << input.substr(5) << std::endl;
-    } else if (input.substr(0, 5) == "type ") {
-      std::string arg = input.substr(5);
-      if (arg == "exit" || arg == "echo" || arg == "type") {
-        std::cout << arg << " is a shell builtin" << std::endl;
-      } else {
-        std::string path = find_path(arg);
-        if (!path.empty()) {
-          std::cout << arg << " is " << path << std::endl;
-        } else {
-          std::cout << arg << ": not found" << std::endl;
-        }
-      }
-    } else {
-      std::cout << input << ": command not found" << std::endl;
-    }
-  }
-  
-  return 0;
+		if (input == "exit 0" || input == "exit") {
+			return 0; // gtfo
+		} else if (input.substr(0, 5) == "echo ") {
+			std::cout << input.substr(5) << std::endl;
+		} else if (input.substr(0, 5) == "type ") {
+			std::string arg = input.substr(5);
+			if (arg == "exit" || arg == "echo" || arg == "type") {
+				std::cout << arg << " is a shell builtin" << std::endl;
+			} else {
+				std::string path = find_path(arg);
+				if (!path.empty()) {
+					std::cout << arg << " is " << path << std::endl;
+				} else {
+					std::cout << arg << ": not found" << std::endl;
+				}
+			}
+		} else {
+			std::cout << input << ": command not found" << std::endl;
+		}
+	}
+
+	return 0;
 }
