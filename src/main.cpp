61a89d7c89803e45cba6222dd2176fff8c93d60e
im-lunex <thississamir04@gmail.com>
2026-03-28 14:24:16 +0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 int main() {
   std::cout << std::unitbuf;
@@ -13,11 +15,20 @@ int main() {
       break; 
     }
 
-    if (input == "exit") {
-      break; // holly shit, just get out
+    if (input.empty()) continue;
+
+    if (input == "exit 0" || input == "exit") {
+      return 0; // gtfo, holly shit
     } else if (input.substr(0, 5) == "echo ") {
       std::cout << input.substr(5) << std::endl;
-    } else if (!input.empty()) {
+    } else if (input.substr(0, 5) == "type ") {
+      std::string arg = input.substr(5);
+      if (arg == "exit" || arg == "echo" || arg == "type") {
+        std::cout << arg << " is a shell builtin" << std::endl;
+      } else {
+        std::cout << arg << ": not found" << std::endl; // wtf is this
+      }
+    } else {
       std::cout << input << ": command not found" << std::endl;
     }
   }
